1e111f6ec460cf21d7146b9bff99105de0147412
Joao Neto <neto.com@msn.com>
2020-05-15 16:26:40 -0300

    Consolidado de renda

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -11,6 +11,7 @@ public class Programa {
 		Scanner sc = new Scanner(System .in);		
 		
 		double rendaAnualComSalario, rendaAnualComPrestacaoServico, rendaAnualComGanhoDeCapital, gastosMedicos, gastosEducacionais;
+		double impostoSobreSalario, impostoSobrePrestaoServico, impostoCapital;
 		
 		//Leitura dos dados
 		
@@ -28,8 +29,55 @@ public class Programa {
 		
 		System.out.print("Gastos educacionais: ");		
 		gastosEducacionais = sc.nextDouble();
+		
+		//Consolidado de renda
+		
+		if(rendaAnualComSalario / 12 < 3000) {
+			
+			impostoSobreSalario = 0.0;
+			
+		} else if(rendaAnualComSalario / 12 >= 3000 && rendaAnualComSalario / 12  < 5000) {
+			
+			impostoSobreSalario = (rendaAnualComSalario * 10 ) / 100;
+			
+		} else {
+			
+			impostoSobreSalario = (rendaAnualComSalario * 20 ) / 100;
+			
+		}
+		
+		
+		if(rendaAnualComPrestacaoServico > 0.0) {
+			
+			impostoSobrePrestaoServico =  (rendaAnualComPrestacaoServico * 15 ) / 100;
+			
+		} else {
+			
+			impostoSobrePrestaoServico = 0.0;
+		}
+		
+		
+		if(rendaAnualComGanhoDeCapital > 0.0) {
+			
+			impostoCapital = (rendaAnualComGanhoDeCapital * 20) / 100;
+			
+		} else {
+			
+			impostoCapital = 0.0;
+			
+		}
+		
+		
+		System.out.println();
+		System.out.println("RELATÓRIO DE IMPOSTO DE RENDA");
+		System.out.println();
+		System.out.println ("CONSOLIDADO DE RENDA: ");
+		System.out.printf("Imposto sobre salário: R$ %.2f%n", impostoSobreSalario);
+		System.out.printf("Imposto sobre serviços: R$ %.2f%n", impostoSobrePrestaoServico);
+		System.out.printf("Imposto sobre ganho de capital: R$ %.2f%n", impostoCapital);
 
 	}
 
 }
+
 
